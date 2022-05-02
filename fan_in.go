42537408486f52dc27dfa08271f3d028a94da0cd
Marc Hayes <marc@hayes.dev>
2022-05-01 18:53:32 -0600

    FanIn* TODO

---
+++ fan_in.go
@@ -0,0 +1,5 @@
+package pipes
+
+// TODO: implement FanIn handler.
+// Do we do this as N-to-1 where there are N goroutines or 1 goroutine looping over incomming channels?
+// Maybe implement both and simplify to one implementation for v1?
