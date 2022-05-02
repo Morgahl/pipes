0a8c90c99d30a2c537723be6b252bf5d23d84c69
Marc Hayes <marc@hayes.dev>
2022-05-01 20:29:36 -0600

    Filter* implemented

--- filter.go
+++ filter.go
@@ -2,13 +2,13 @@ package pipes
 
 import "github.com/curlymon/pipes/fn"
 
-func Filter[T any](size int, filter fn.Filter[T], in <-chan T) <-chan T {
+func Filter[T any](size int, filter fn.Filter[T], in ChanPull[T]) ChanPull[T] {
 	out := make(chan T, size)
 	go filterWorker(filter, in, out)
 	return out
 }
 
-func filterWorker[T any](filter fn.Filter[T], in <-chan T, out chan<- T) {
+func filterWorker[T any](filter fn.Filter[T], in ChanPull[T], out ChanPush[T]) {
 	defer close(out)
 	for t := range in {
 		if filter(t) {
@@ -17,7 +17,7 @@ func filterWorker[T any](filter fn.Filter[T], in <-chan T, out chan<- T) {
 	}
 }
 
-func FilterWitError[T any](size int, filter fn.FilterWithError[T], in <-chan T) (<-chan T, <-chan error) {
+func FilterWithError[T any](size int, filter fn.FilterWithError[T], in <-chan T) (<-chan T, <-chan error) {
 	out, err := make(chan T, size), make(chan error, size)
 	go filterWithErrorWorker(filter, in, out, err)
 	return out, err
@@ -34,7 +34,7 @@ func filterWithErrorWorker[T any](filter fn.FilterWithError[T], in <-chan T, out
 	}
 }
 
-func FilterWitErrorSink[T any](size int, filter fn.FilterWithError[T], sink fn.Sink[error], in <-chan T) <-chan T {
+func FilterWithErrorSink[T any](size int, filter fn.FilterWithError[T], sink fn.Sink[error], in <-chan T) <-chan T {
 	out := make(chan T, size)
 	go filterWithErrorSinkWorker(filter, sink, in, out)
 	return out
