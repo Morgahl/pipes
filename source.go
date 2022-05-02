5f34af068c95efcdd2855726b85834e70e48bc67
Marc Hayes <marc@hayes.dev>
2022-05-01 18:53:03 -0600

    Source* now using discrete workers

--- source.go
+++ source.go
@@ -6,41 +6,47 @@ const RepeatForever = -1
 
 func Source[T any](repeat, size int, source fn.Source[T]) <-chan T {
 	out := make(chan T, size)
-	go func() {
-		defer close(out)
-		for i := 0; repeat == RepeatForever || repeat > 0; i++ {
-			out <- source()
-		}
-	}()
+	go sourceWorker(repeat, source, out)
 	return out
+}
+
+func sourceWorker[T any](repeat int, source fn.Source[T], out chan<- T) {
+	defer close(out)
+	for i := 0; repeat == RepeatForever || repeat > 0; i++ {
+		out <- source()
+	}
 }
 
 func SourceWithError[T any](repeat, size int, source fn.SourceWithError[T]) (<-chan T, <-chan error) {
 	out, err := make(chan T, size), make(chan error, size)
-	go func() {
-		defer func() { close(err); close(out) }()
-		for i := 0; repeat == RepeatForever || repeat > 0; i++ {
-			if v, er := source(); er != nil {
-				err <- er
-			} else {
-				out <- v
-			}
-		}
-	}()
+	go sourceWithErrorWorker(repeat, source, out, err)
 	return out, err
+}
+
+func sourceWithErrorWorker[T any](repeat int, source fn.SourceWithError[T], out chan<- T, err chan<- error) {
+	defer func() { close(err); close(out) }()
+	for i := 0; repeat == RepeatForever || repeat > 0; i++ {
+		if v, er := source(); er != nil {
+			err <- er
+		} else {
+			out <- v
+		}
+	}
 }
 
 func SourceWithErrorSink[T any](repeat, size int, source fn.SourceWithError[T], sink fn.Sink[error]) <-chan T {
 	out := make(chan T, size)
-	go func() {
-		defer close(out)
-		for i := 0; repeat == RepeatForever || repeat > 0; i++ {
-			if v, err := source(); err != nil {
-				sink(err)
-			} else {
-				out <- v
-			}
-		}
-	}()
+	go sourceWithErrorSinkWorker(repeat, source, sink, out)
 	return out
+}
+
+func sourceWithErrorSinkWorker[T any](repeat int, source fn.SourceWithError[T], sink fn.Sink[error], out chan<- T) {
+	defer close(out)
+	for i := 0; repeat == RepeatForever || repeat > 0; i++ {
+		if v, err := source(); err != nil {
+			sink(err)
+		} else {
+			out <- v
+		}
+	}
 }
