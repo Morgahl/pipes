bdc5e5d2507685b08943e2c8407ac00f94aa4db5
Marc Hayes <marc@hayes.dev>
2022-05-06 03:20:42 -0600

    document FanIn internals

--- fan_in.go
+++ fan_in.go
@@ -4,11 +4,12 @@ import "sync"
 
 // FanIn is a non-blocking operation that creates len(ins) goroutines and forwards each T read onto
 // the returned push only channel of specified size. Each goroutine will exit after it's assigned
-// input channel is closed and emptied. The last goroutine will close the returned channel to signal
-// completion of work.
+// input channel is closed and emptied. The last goroutine will close the returned pull only channel
+// to signal completion of processing.
 func FanIn[T any](size int, ins ...<-chan T) ChanPull[T] {
 	out := make(chan T, size)
 	if len(ins) < 1 {
+		// Let's never return a nil channel, a close empty channel has better behaviors
 		close(out)
 		return out
 	}
@@ -18,11 +19,15 @@ func FanIn[T any](size int, ins ...<-chan T) ChanPull[T] {
 	return out
 }
 
+// fanInCoordinator will create len(ins)-1 fanInWorkers then demote itself to a fanInWorker, ensure
+// that at least 1 push only channel is passed or the function will panic. This will close the
+// passed push only channel after the last worker exits.
 func fanInCoordinator[T any](ins []<-chan T, out chan<- T) {
 	defer close(out)
 
 	wg := &sync.WaitGroup{}
 	wg.Add(len(ins))
+	// skipping the first create a worker for each passed pull only channel
 	for _, in := range ins[1:] {
 		go fanInWorker(wg, in, out)
 	}
@@ -34,6 +39,8 @@ func fanInCoordinator[T any](ins []<-chan T, out chan<- T) {
 	wg.Wait()
 }
 
+// fanInWorker iterates over the passed pull only channel forwarding values to the passed push only
+// channel. Worker will exit after the pul only channel is closed and emptied.
 func fanInWorker[T any](wg *sync.WaitGroup, in <-chan T, out chan<- T) {
 	defer wg.Done()
 
